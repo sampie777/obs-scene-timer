caba10b693199600905b0305d94555123aa12cb4
S. Jansen <sa.jansen1@gmail.com>
2020-05-13 22:19:43 +0200

    Added logService to pipe logging to file

--- src/main/kotlin/application.kt
+++ src/main/kotlin/application.kt
@@ -1,14 +1,28 @@
 import config.Config
 import gui.MainFrame
 import objects.OBSClient
+import objects.notifications.Notifications
 import themes.Theme
 import java.awt.EventQueue
+import java.util.logging.Level
+import java.util.logging.LogRecord
 import java.util.logging.Logger
+import kotlin.Exception
 
 fun main(args: Array<String>) {
     val logger = Logger.getLogger("Application")
     logger.info("Starting application")
+    LogService.logBuffer.add(LogRecord(Level.INFO, "Starting application"))
     logger.info("Executing JAR directory: " + getCurrentJarDirectory(Config).absolutePath)
+    LogService.logBuffer.add(LogRecord(Level.INFO, "Executing JAR directory: " + getCurrentJarDirectory(Config).absolutePath))
+
+    try {
+        LogService.setup(args)
+    } catch (e: Exception) {
+        logger.severe("Failed to initiate logging: $e")
+        e.printStackTrace()
+        Notifications.add("Failed to setup logging service", "Application")
+    }
 
     Config.enableWriteToFile(true)
     Config.load()
