1a8e58cc52d803ba8fef3e60e0a5d7eb6d8743c1
S. Jansen <sa.jansen1@gmail.com>
2020-05-14 14:16:07 +0200

    Launch4j exe is now build using Maven. Also, application information is connected with Maven.

---
+++ src/main/kotlin/objects/ApplicationInfo.kt
@@ -0,0 +1,23 @@
+package objects
+
+import gui.MainFrame
+import java.util.*
+
+object ApplicationInfo {
+    private val properties = Properties()
+    val version: String
+    val artifactId: String
+    val name: String
+    val author: String
+    val url: String
+
+    init {
+        properties.load(MainFrame::class.java.getResourceAsStream("/application.properties"))
+        version = properties.getProperty("version")
+        artifactId = properties.getProperty("artifactId")
+        name = properties.getProperty("name")
+        author = properties.getProperty("author")
+        url = properties.getProperty("url")
+    }
+
+}
