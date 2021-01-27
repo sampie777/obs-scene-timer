7e296ce46ecd606c282c9718668d13e932f2da5d
S. Jansen <sa.jansen1@gmail.com>
2021-01-27 09:49:44 +0100

    Check for updates can be disabled from the update popup.

--- src/main/kotlin/gui/updater/UpdatePopupActionPanel.kt
+++ src/main/kotlin/gui/updater/UpdatePopupActionPanel.kt
@@ -1,21 +1,23 @@
 package gui.updater
 
+import config.Config
 import objects.ApplicationInfo
 import objects.notifications.Notifications
+import themes.Theme
 import java.awt.Desktop
 import java.awt.Dimension
+import java.awt.Font
 import java.awt.event.KeyEvent
 import java.io.IOException
 import java.net.URI
 import java.util.logging.Logger
-import javax.swing.Box
-import javax.swing.BoxLayout
-import javax.swing.JButton
-import javax.swing.JPanel
+import javax.swing.*
 import javax.swing.border.EmptyBorder
 
 class UpdatePopupActionPanel(private val frame: UpdatePopup) : JPanel() {
     private val logger = Logger.getLogger(UpdatePopupActionPanel::class.java.name)
+
+    private val disableUpdateCheckerCheckbox = JCheckBox()
 
     init {
         createGui()
@@ -24,6 +26,11 @@ class UpdatePopupActionPanel(private val frame: UpdatePopup) : JPanel() {
     private fun createGui() {
         layout = BoxLayout(this, BoxLayout.LINE_AXIS)
         border = EmptyBorder(0, 10, 10, 10)
+
+        disableUpdateCheckerCheckbox.text = "Don't check for future updates"
+        disableUpdateCheckerCheckbox.toolTipText = "This can be re-enabled in the settings screen"
+        disableUpdateCheckerCheckbox.font = Font(Theme.get.FONT_FAMILY, Font.PLAIN, 12)
+        disableUpdateCheckerCheckbox.isSelected = !Config.updatesCheckForUpdates
 
         val closeButton = JButton("Close")
         closeButton.addActionListener { closePopup() }
@@ -35,6 +42,7 @@ class UpdatePopupActionPanel(private val frame: UpdatePopup) : JPanel() {
         downloadButton.mnemonic = KeyEvent.VK_D
         frame.rootPane.defaultButton = downloadButton
 
+        add(disableUpdateCheckerCheckbox)
         add(Box.createHorizontalGlue())
         add(closeButton)
         add(Box.createRigidArea(Dimension(10, 0)))
@@ -43,6 +51,11 @@ class UpdatePopupActionPanel(private val frame: UpdatePopup) : JPanel() {
 
     private fun closePopup() {
         logger.info("Closing update window")
+
+        if (Config.updatesCheckForUpdates != !disableUpdateCheckerCheckbox.isSelected) {
+            logger.info("Updating Config.updatesCheckForUpdates to ${!disableUpdateCheckerCheckbox.isSelected}")
+        }
+        Config.updatesCheckForUpdates = !disableUpdateCheckerCheckbox.isSelected
 
         frame.dispose()
     }
